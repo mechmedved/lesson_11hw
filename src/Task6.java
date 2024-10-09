4efd889cb5e9959274d33684c79ba3cf4d796c89
mechmedved <unclebear@yandex.ru>
2024-10-09 14:08:47 +0300

    Выполнил рефактор метода findNearestLongerOrShortestLine()

--- src/Task6.java
+++ src/Task6.java
@@ -6,49 +6,44 @@ public class Task6 {
         ArrayList<String> myList = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
-        for(int i=0;i<5;i++){
+        for(int i=0;i<5;i++)
+        {
             myList.add(sc.nextLine());
         }
 
         sc.close();
 
-        int indexMax = myList.indexOf(maxString(myList));
-        int indexMin = myList.indexOf(minString(myList));
-
-        if(indexMax<indexMin){
-            System.out.println(myList.get(indexMax));
-        }
-        else{
-            System.out.println(myList.get(indexMin));
-        }
-
+        System.out.println(findNearestLongerOrShortestLine(myList));
     }
 
-    private static String maxString(ArrayList<String> myList){
+    private static String findNearestLongerOrShortestLine(ArrayList<String> myList)
+    {
+        if (myList.isEmpty()) {
+            throw new IllegalArgumentException("List must not be empty");
+        }
 
-        int maxValue = myList.getFirst().length();
         String maxString = myList.getFirst();
-        for(var value: myList){
-            int currentLength = value.length();
-            if(currentLength>maxValue){
-                maxString = value;
-            }
-        }
-
-        return maxString;
-    }
-
-    private static String minString(ArrayList<String> myList){
-
-        int minValue = myList.getFirst().length();
         String minString = myList.getFirst();
-        for(var value: myList){
-            int currentLength = value.length();
-            if(currentLength<minValue){
-                minString = value;
+        int maxIndex = 0;
+        int minIndex = 0;
+
+        for (int i = 1; i < myList.size(); i++)
+        {
+            int currentLength = myList.get(i).length();
+
+            if (currentLength > maxString.length())
+            {
+                maxString = myList.get(i);
+                maxIndex = i;
+            }
+
+            if (currentLength < minString.length())
+            {
+                minString = myList.get(i);
+                minIndex = i;
             }
         }
 
-        return minString;
+        return (maxIndex < minIndex) ? maxString : minString;
     }
 }
